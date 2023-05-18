95598152e24966dd3ae19258441ccf6a92605e81
Asad Yasin <51986208+asadyasin@users.noreply.github.com>
2023-05-18 18:17:18 +0500

    jwt token added

--- server/controller/appController.js
+++ server/controller/appController.js
@@ -1,14 +1,62 @@
 import UserModel from "../models/User.model.js"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-/** POST: http://localhost:5000/api/register */
+/** POST: http://localhost:8080/api/register */
 
-export async function register(req, res){
-    res.json('Register Route')
-}
+export async function register(req, res) {
+    try {
+      const { username, password, profile, email } = req.body;
+  
+      // Check if username or email already exists
+      const existingUser = await UserModel.findOne().or([{ username }, { email }]);
+      if (existingUser) {
+        return res.status(400).send({ error: "Username or email already exists" });
+      }
+  
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      // Create a new user
+      const user = new UserModel({
+        username,
+        password: hashedPassword,
+        profile: profile || "",
+        email,
+      });
+  
+      // Save the user
+      const savedUser = await user.save();
+  
+      res.status(201).send({ msg: "User registered successfully", user: savedUser });
+    } catch (error) {
+      res.status(500).send({ error: error.message });
+    }
+  }
+  
 
 /** POST: /api/login */
 export async function login(req, res){
-    res.json('Login Route')
+    try {
+      const {email,password} = req.body;
+
+      UserModel.findOne({email})
+      .then( user=>{
+        bcrypt.compare(password, user.password)
+          .then(passwordChecked=>{
+            if(!passwordChecked) return res.status(400).send({ error: "Don't have password"})
+            
+            // create jwt token
+            jwt.sign()
+            
+          })
+          .catch(error =>{res.status(400).send({ error: "Password does Not Match"})})
+      })
+      .catch(error =>{res.status(400).send({ error: "User Not Found"})})
+
+    } catch (error) {
+      res.status(500).send({ error: error.message });
+    }
 }
 
 /** GET: /api/user/name */
